d70a69af878cb55d1dd0ed0d81360e77c33bc9cc
Harjot Sandhu <harjot.sandhu@guidanceresidential.com>
2018-03-20 13:16:55 -0400

    Add Google Authentication.

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent{
+  user$: Observable<firebase.User>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth) {
+    this.user$ = afAuth.authState;
   }
 
+  logout(){
+    this.afAuth.auth.signOut();
+  }
 }
